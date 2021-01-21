50cd337524687edc1525ab5d0eca24810ee46431
Ray Lee <ray.lee@lyrasis.org>
2021-01-21 12:29:38 -0500

    Customize concept.

---
+++ src/plugins/recordTypes/concept/fields.js
@@ -0,0 +1,114 @@
+import { defineMessages } from 'react-intl';
+
+export default (configContext) => {
+  const {
+    AutocompleteInput,
+    TextInput,
+    TermPickerInput,
+  } = configContext.inputComponents;
+
+  const {
+    configKey: config,
+  } = configContext.configHelpers;
+
+  return {
+    document: {
+      'ns2:concepts_common': {
+        conceptTermGroupList: {
+          conceptTermGroup: {
+            termType: {
+              [config]: {
+                view: {
+                  type: TermPickerInput,
+                  props: {
+                    source: 'termtype',
+                  },
+                },
+              },
+            },
+            historicalStatus: {
+              [config]: {
+                view: {
+                  type: TermPickerInput,
+                  props: {
+                    source: 'historicalStatus',
+                  },
+                },
+              },
+            },
+          },
+        },
+        citationGroupList: {
+          citationGroup: {
+            citationSource: {
+              [config]: {
+                view: {
+                  type: AutocompleteInput,
+                  props: {
+                    source: 'citation/local',
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+      'ns2:concepts_omca': {
+        [config]: {
+          service: {
+            ns: 'http://collectionspace.org/services/concept/local/omca',
+          },
+        },
+        legacyArgusNotes: {
+          [config]: {
+            messages: defineMessages({
+              name: {
+                id: 'field.concepts_omca.legacyArgusNotes.name',
+                defaultMessage: 'Legacy Argus notes',
+              },
+            }),
+            view: {
+              type: TextInput,
+              props: {
+                multiline: true,
+                readOnly: true,
+              },
+            },
+          },
+        },
+        legacyParents: {
+          [config]: {
+            messages: defineMessages({
+              name: {
+                id: 'field.concepts_omca.legacyParents.name',
+                defaultMessage: 'Legacy parents',
+              },
+            }),
+            view: {
+              type: TextInput,
+              props: {
+                readOnly: true,
+              },
+            },
+          },
+        },
+        legacySeeAlso: {
+          [config]: {
+            messages: defineMessages({
+              name: {
+                id: 'field.concepts_omca.legacySeeAlso.name',
+                defaultMessage: 'Legacy see also',
+              },
+            }),
+            view: {
+              type: TextInput,
+              props: {
+                readOnly: true,
+              },
+            },
+          },
+        },
+      },
+    },
+  };
+};
--- src/plugins/recordTypes/index.js
+++ src/plugins/recordTypes/index.js
@@ -2,10 +2,12 @@ import acquisition from './acquisition';
 import citation from './citation';
 import claim from './claim';
 import collectionobject from './collectionobject';
+import concept from './concept';
 
 export default [
   acquisition,
   citation,
   claim,
   collectionobject,
+  concept,
 ];
